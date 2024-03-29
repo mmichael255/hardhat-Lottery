28aa862e9497014031d1873571706923e4548955
mmichael255 <cby371@gmail.com>
2024-03-29 23:13:01 +0900

    init

---
+++ hardhat.config.ts
@@ -0,0 +1,9 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config"
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.7",
+};
+
+export default config;
