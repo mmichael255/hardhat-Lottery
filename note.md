3dfd8426d5503ebe9ded02a3b3161f04e704107c
mmichael255 <cby371@gmail.com>
2024-04-02 22:37:43 +0900

    testStaticCall

--- note.md
+++ note.md
@@ -1,5 +1,7 @@
-//to get a return value from TransactionResponse
+##to get a return value from TransactionResponse
 const transactionResponse = await mock.createSubscription()
 const transactionReceipt = await transactionResponse.wait(1)
 console.log(transactionReceipt.toJSON())
 console.log(transactionReceipt!.logs[0].args[0])
+OR
+const subId = await mock.createSubscription.staticCall()
--- test/testRaffle.ts
+++ test/testRaffle.ts
@@ -95,10 +95,12 @@ import { Raffle, VRFCoordinatorV2Mock } from "../typechain-types"
                   const { raffle, mock, signers, enteranceFee, interval } =
                       await getContract()
                   //to get a return value from TransactionResponse
-                  const transactionResponse = await mock.createSubscription()
-                  const transactionReceipt = await transactionResponse.wait(1)
-                  console.log(transactionReceipt.toJSON())
-                  console.log(transactionReceipt!.logs[0].args[0])
+                  //   const transactionResponse = await mock.createSubscription()
+                  //   const transactionReceipt = await transactionResponse.wait(1)
+                  //   console.log(transactionReceipt.toJSON())
+                  //   console.log(transactionReceipt!.logs[0].args[0])
+                  const subId = await mock.createSubscription.staticCall()
+                  console.log(subId)
 
                   const playerraffle = raffle.connect(signers[1])
                   await playerraffle.enterRaffle({
